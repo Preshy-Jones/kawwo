dea45c32774f6cb63c0bc8e6e6fdacfd9cdd95fc
Precious Adedibu <adedibuprecious@gmail.com>
2024-01-26 13:10:24 +0100

    update

--- components/layouts/NavBar.tsx
+++ components/layouts/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import kawwoLogo from "../../public/kawwo-logo.svg";
 import Image from "next/image";
 import Hamburger from "../../public/hamburger.svg";
+import Link from "next/link";
 
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -11,11 +12,21 @@ const NavBar = () => {
         <div className=" flex items-center justify-between">
           <Image src={kawwoLogo} alt="kawwo-logo" className="" />
           <ul className="lg:flex justify-around w-[50%] hidden">
-            <li className="">Products</li>
-            <li className="">Use cases</li>
-            <li className="">Pricing</li>
-            <li className="">About us</li>
-            <li className="">Consumers</li>
+            <Link href={"#"} className="">
+              Products
+            </Link>
+            <Link href={"#"} className="">
+              Use cases
+            </Link>
+            <Link href={"#"} className="">
+              Pricing
+            </Link>
+            <Link href={"/about"} className="">
+              About us
+            </Link>
+            <Link href={"#"} className="">
+              Consumers
+            </Link>
           </ul>
           <div className="lg:flex items-center hidden">
             <h2 className="text-primary font-bold leading-[1.361875rem] mr-5">
@@ -35,11 +46,21 @@ const NavBar = () => {
         {showMenu && (
           <div className="mt-5 absolute bg-white w-full py-5">
             <ul className="grid justify-items-center gap-y-2">
-              <li className="">Products</li>
-              <li className="">Use cases</li>
-              <li className="">Pricing</li>
-              <li className="">About us</li>
-              <li className="">Consumers</li>
+              <Link href={"#"} className="">
+                Products
+              </Link>
+              <Link href={"#"} className="">
+                Use cases
+              </Link>
+              <Link href={"#"} className="">
+                Pricing
+              </Link>
+              <Link href={"/about"} className="">
+                About us
+              </Link>
+              <Link href={"#"} className="">
+                Consumers
+              </Link>
             </ul>
             <div className="flex flex-col items-center mt-3">
               <h2 className="text-primary font-bold leading-[1.361875rem] mb-3">
