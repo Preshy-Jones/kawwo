5564b3472c12db8f754718a0e97c431e6793f51e
Precious Adedibu <adedibuprecious@gmail.com>
2024-01-26 13:05:22 +0100

    update

--- components/layouts/NavBar.tsx
+++ components/layouts/NavBar.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 import Hamburger from "../../public/hamburger.svg";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="w-full  py-3 flex justify-center ">
-      <div className="w-[82.22%]">
+      <div className="w-[82.22%] relative">
         <div className=" flex items-center justify-between">
           <Image src={kawwoLogo} alt="kawwo-logo" className="" />
           <ul className="lg:flex justify-around w-[50%] hidden">
@@ -24,29 +25,32 @@ const NavBar = () => {
               Sign in
             </button>
           </div>
-          <Image
-            src={Hamburger}
-            alt="hamburger-icon"
-            className="lg:hidden cursor-pointer"
-          />
-        </div>
-        <div className="mt-5">
-          <ul className="grid justify-items-center gap-y-2">
-            <li className="">Products</li>
-            <li className="">Use cases</li>
-            <li className="">Pricing</li>
-            <li className="">About us</li>
-            <li className="">Consumers</li>
-          </ul>
-          <div className="flex flex-col items-center mt-3">
-            <h2 className="text-primary font-bold leading-[1.361875rem] mb-3">
-              Create an account
-            </h2>
-            <button className=" border-primaryc border rounded-md py-2 px-7">
-              Sign in
-            </button>
+          <div
+            onClick={() => setShowMenu(!showMenu)}
+            className="cursor-pointer"
+          >
+            <Image src={Hamburger} alt="hamburger-icon" className="lg:hidden" />
           </div>
         </div>
+        {showMenu && (
+          <div className="mt-5 absolute bg-white w-full py-5">
+            <ul className="grid justify-items-center gap-y-2">
+              <li className="">Products</li>
+              <li className="">Use cases</li>
+              <li className="">Pricing</li>
+              <li className="">About us</li>
+              <li className="">Consumers</li>
+            </ul>
+            <div className="flex flex-col items-center mt-3">
+              <h2 className="text-primary font-bold leading-[1.361875rem] mb-3">
+                Create an account
+              </h2>
+              <button className=" border-primaryc border rounded-md py-2 px-7">
+                Sign in
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
